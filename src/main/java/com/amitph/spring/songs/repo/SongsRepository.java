7dfb3b5c6d61cf726cd6af7775b9fdede27cb957
Amit Phaltankar <amit.phaltankar@objective.com>
2018-12-16 08:03:12 +1100

    Added LongKeySong with @EmbeddedId

--- src/main/java/com/amitph/spring/songs/repo/SongsRepository.java
+++ src/main/java/com/amitph/spring/songs/repo/SongsRepository.java
@@ -3,5 +3,9 @@ package com.amitph.spring.songs.repo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SongsRepository extends CrudRepository<Song, SongId> {}
+public interface SongsRepository extends CrudRepository<Song, SongId> {
+    List<Song> findByIdNameAndIdArtist(String name, String artist);
+}
